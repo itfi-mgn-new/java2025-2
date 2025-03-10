7d710f314031ff524ef59af82d0f709167b066a4
student <student@itf-122-teacher.itf.local>
2025-03-10 19:47:30 +0500

    DAily commit

--- src/lesson1/HelloWorld.java
+++ src/lesson1/HelloWorld.java
@@ -1,10 +1,23 @@
 package lesson1;
 
-public class HelloWorld extends java.lang.Object {
+import java.lang.*;
+import lesson1.*;
 
+public class HelloWorld /*extends java.lang.Object*/ {
+	public static int	x = 10;
+	
+	public int			y = 20; 
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		java.lang.System.err.println("Hello world");
+		int		z = 909;
+		
+		byte	b1 = 100, b2 = 100, b3 = (byte)(b1 + b2);
+		
+		System.err.println("b3="+b3);
+		
+//		System.err.println("Hello world");
 	}
 
 }
