4c070ac0326e5c0e8a527557e31b35fce2d58b93
student <student@itf-122-teacher.itf.local>
2025-03-10 19:50:50 +0500

    DAily commit

--- src/lesson1/HelloWorld.java
+++ src/lesson1/HelloWorld.java
@@ -17,6 +17,23 @@ public class HelloWorld /*extends java.lang.Object*/ {
 		
 		System.err.println("b3="+b3);
 		
+		int		i1 = 10, i2 = 0xbf0e, i3 = 0b0010, i4 = 077;
+		long	long1 = 123_456_7_8__900_1234_45L;
+		float	f2 = 1.5f;
+		double	d1 = 1.5e22d;
+		char	c1 = 13, c2 = '\n', c3 = '\015', c4 = '\u24FE';
+		
+		
+		long	val1 = 12345, val2 = 54321;
+		double	vald1 = 10, vald2 = 20;
+		
+		boolean	bool1 = true, bool2 = false;
+		
+		int		zzz = (bool1 ? (10) : (-10));
+		
+		
+		
+		
 //		System.err.println("Hello world");
 	}
 
