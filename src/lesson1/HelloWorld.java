53699265c4bb241d704cad58487dc19c332e4fa4
chav1961 <chav1961@mail.ru>
2025-03-10 12:13:05 +0500

    Initial commit

---
+++ src/lesson1/HelloWorld.java
@@ -0,0 +1,10 @@
+package lesson1;
+
+public class HelloWorld extends java.lang.Object {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		java.lang.System.err.println("Hello world");
+	}
+
+}
